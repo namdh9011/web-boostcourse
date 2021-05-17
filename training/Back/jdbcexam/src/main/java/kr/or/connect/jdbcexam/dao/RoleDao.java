bccce1855ba6e8c65e73cb0f731735c76b4a652e
남동해 <namdh0911@gmail.com>
2021-05-17 18:16:33 +0900

    project: 2-3강 selectlist 실습

--- training/Back/jdbcexam/src/main/java/kr/or/connect/jdbcexam/dao/RoleDao.java
+++ training/Back/jdbcexam/src/main/java/kr/or/connect/jdbcexam/dao/RoleDao.java
@@ -5,6 +5,8 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import kr.or.connect.jdbcexam.dto.Role;
 
@@ -85,4 +87,35 @@ public class RoleDao {
 		}
 		return insertCount;
 	}
+	
+	public List<Role> getRoles() {
+		List<Role> list = new ArrayList<>();
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		String sql = "SELECT description, role_id FROM role order by role_id desc";
+		try (Connection conn = DriverManager.getConnection(dburl, dbUser, dbpassword);
+				PreparedStatement ps = conn.prepareStatement(sql)) {
+
+			try (ResultSet rs = ps.executeQuery()) {
+
+				while (rs.next()) {
+					String description = rs.getString(1);
+					int id = rs.getInt("role_id");
+					Role role = new Role(id, description);
+					list.add(role); // list에 반복할때마다 Role인스턴스를 생성하여 list에 추가한다.
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		return list;
+	}
+	
 }
