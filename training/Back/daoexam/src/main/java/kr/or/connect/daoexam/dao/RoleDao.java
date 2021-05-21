154ac28c08b13c13929955a45bee85044724fad5
남동해 <namdh0911@gmail.com>
2021-05-22 01:51:46 +0900

    project: 실습 3 selectall 실습

---
+++ training/Back/daoexam/src/main/java/kr/or/connect/daoexam/dao/RoleDao.java
@@ -0,0 +1,34 @@
+package kr.or.connect.daoexam.dao;
+
+import java.util.Collections;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Repository;
+
+import kr.or.connect.daoexam.dto.Role;
+
+import static kr.or.connect.daoexam.dao.RoleDaoSqls.*;	//static import 사용 
+@Repository
+public class RoleDao {
+	private NamedParameterJdbcTemplate jdbc;	//이름을 이용해서 바인딩 하거나 객체 값을 가져올 수 있게해줌
+	private SimpleJdbcInsert insertAction;
+	
+	//java는 필드에 roleId와 같은 카멜 표기법을 쓰고 MySQL은 role_id와 같은 방식으로 네이밍을 해주는데 두 필드 값이 달라도 알아서 네이밍 해준다.
+	private RowMapper<Role> rowMapper = BeanPropertyRowMapper.newInstance(Role.class);
+
+	public RoleDao(DataSource dataSource) {
+		this.jdbc = new NamedParameterJdbcTemplate(dataSource);
+		this.insertAction = new SimpleJdbcInsert(dataSource)
+                .withTableName("role");
+	}
+	
+	public List<Role> selectAll(){
+		return jdbc.query(SELECT_ALL, Collections.emptyMap(), rowMapper);
+	}
+}
