d0b9f4edcb6f8c8295e6768a94442a7074188861
남동해 <namdh0911@gmail.com>
2021-05-22 02:05:27 +0900

    project: insert Update 추가

--- training/Back/daoexam/src/main/java/kr/or/connect/daoexam/dao/RoleDao.java
+++ training/Back/daoexam/src/main/java/kr/or/connect/daoexam/dao/RoleDao.java
@@ -7,7 +7,9 @@ import javax.sql.DataSource;
 
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
@@ -17,7 +19,7 @@ import static kr.or.connect.daoexam.dao.RoleDaoSqls.*;	//static import 사용
 @Repository
 public class RoleDao {
 	private NamedParameterJdbcTemplate jdbc;	//이름을 이용해서 바인딩 하거나 객체 값을 가져올 수 있게해줌
-	private SimpleJdbcInsert insertAction;
+	private SimpleJdbcInsert insertAction;		//Insert를 위해 필요
 	
 	//java는 필드에 roleId와 같은 카멜 표기법을 쓰고 MySQL은 role_id와 같은 방식으로 네이밍을 해주는데 두 필드 값이 달라도 알아서 네이밍 해준다.
 	private RowMapper<Role> rowMapper = BeanPropertyRowMapper.newInstance(Role.class);
@@ -25,10 +27,20 @@ public class RoleDao {
 	public RoleDao(DataSource dataSource) {
 		this.jdbc = new NamedParameterJdbcTemplate(dataSource);
 		this.insertAction = new SimpleJdbcInsert(dataSource)
-                .withTableName("role");
+                .withTableName("role");		//insert 어떤 테이블에 넣을지 알려줌.
 	}
 	
 	public List<Role> selectAll(){
 		return jdbc.query(SELECT_ALL, Collections.emptyMap(), rowMapper);
+	}
+	
+	public int insert(Role role) {
+		SqlParameterSource params = new BeanPropertySqlParameterSource(role);
+		return insertAction.execute(params);
+	}
+	
+	public int update(Role role) {
+		SqlParameterSource params = new BeanPropertySqlParameterSource(role);
+		return jdbc.update(UPDATE, params);
 	}
 }
--- training/Back/daoexam/src/main/java/kr/or/connect/daoexam/dao/RoleDaoSqls.java
+++ training/Back/daoexam/src/main/java/kr/or/connect/daoexam/dao/RoleDaoSqls.java
@@ -2,5 +2,9 @@ package kr.or.connect.daoexam.dao;
 
 public class RoleDaoSqls {
 	public static final String SELECT_ALL = "SELECT role_id, description FROM role order by role_id";
+	//insert문 같은 경우는 쿼리가 따로 필요하지 않음.
+
+	//:값  값이 바인딩 될 부분이다.
+	public static final String UPDATE = "UPDATE role set description = :description where role_id = :roleId";
 	
 }
