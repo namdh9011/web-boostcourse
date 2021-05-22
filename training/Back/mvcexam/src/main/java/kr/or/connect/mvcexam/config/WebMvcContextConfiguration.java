e2f57f4ba9717e8d3085e2423bbab1d9d578f50c
남동해 <namdh0911@gmail.com>
2021-05-22 17:39:34 +0900

    project: DispatcherServlet, 프론트 핸들러, mainpage 생성

---
+++ training/Back/mvcexam/src/main/java/kr/or/connect/mvcexam/config/WebMvcContextConfiguration.java
@@ -0,0 +1,47 @@
+package kr.or.connect.mvcexam.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {"kr.or.connect.mvcexam.controller"})
+public class WebMvcContextConfiguration extends WebMvcConfigurerAdapter {
+	@Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        //이런 url요청을 받을때는 오른쪽에서 찾아라 이 요청이 없을때는 알아서 컨트롤러에서 찾는다.
+		registry.addResourceHandler("/assets/**").addResourceLocations("classpath:/META-INF/resources/webjars/").setCachePeriod(31556926);
+        registry.addResourceHandler("/css/**").addResourceLocations("/css/").setCachePeriod(31556926);
+        registry.addResourceHandler("/img/**").addResourceLocations("/img/").setCachePeriod(31556926);
+        registry.addResourceHandler("/js/**").addResourceLocations("/js/").setCachePeriod(31556926);
+    }
+ 
+    // default servlet handler를 사용하게 합니다.
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+   
+    //특정 url에 대한 처리를 컨트롤러 작성 없이 하게 해준다.
+    @Override
+    public void addViewControllers(final ViewControllerRegistry registry) {
+    		System.out.println("addViewControllers가 호출됩니다. ");
+        registry.addViewController("/").setViewName("main");
+    }
+    
+    //"/WEB-INF/views/main.jsp" 가 동작하게된다.
+    @Bean
+    public InternalResourceViewResolver getInternalResourceViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+}
